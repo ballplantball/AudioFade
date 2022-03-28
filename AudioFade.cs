64b9f5d67c2254b11b861b6ede905ee7ecdb2144
Tom Hsiao <44054500+ballplantball@users.noreply.github.com>
2022-03-28 23:05:24 +0800

    Add files via upload

---
+++ AudioFade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioFade : MonoBehaviour
+{
+    public static IEnumerator FadeOut(AudioSource audioSource, float FadeTime, float FadeVolume)//聲音漸小到某值
+    {
+        float startVolume = audioSource.volume;
+        while (audioSource.volume > FadeVolume)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+            yield return null;
+        }
+    }
+
+    public static IEnumerator FadeIn(AudioSource audioSource, float FadeTime, float FadeVolume)//聲音漸大到某值
+    {
+        while (audioSource.volume < FadeVolume)
+        {
+            audioSource.volume += 0.2f * Time.deltaTime / FadeTime;
+            yield return null;
+        }
+    }
+}
